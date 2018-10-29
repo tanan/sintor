02cb7ad0f02eadf55a1202436644353fbeec5fe4
Toshifumi Anan <toshifumi.anan@uzabase.com>
2018-10-29 19:29:48 +0900

    add yaml setting

--- config/config.go
+++ config/config.go
@@ -1,26 +1,46 @@
 package config
 
-import "github.com/BurntSushi/toml"
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
 
 type Config struct {
-	Application AppConfig   `toml:"application"`
-	Slack       SlackConfig `toml:"slack"`
+	Application AppConfig `yaml:"application"`
 }
 
 type AppConfig struct {
-	Port int `toml:"port"`
+	Port     int          `toml:"port"`
+	Database DBConfig     `yaml:"database"`
+	Notify   NotifyConfig `yaml:"notify"`
+}
+
+type DBConfig struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"pass"`
+	Host     string `yaml:"host"`
+	Schema   string `yaml:"schema"`
+	Port     int    `yaml:"port"`
+}
+
+type NotifyConfig struct {
+	Slack SlackConfig `yaml:"slack"`
 }
 
 type SlackConfig struct {
-	Token   string `toml:"token"`
-	ApiURL  string `toml:"api_url"`
-	Channel string `toml:"channel"`
-	User    string `toml:"user"`
+	Token   string `yaml:"token"`
+	ApiURL  string `yaml:"api_url"`
+	Channel string `yaml:"channel"`
+	User    string `yaml:"user"`
 }
 
 func LoadFile(path string) (*Config, error) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
 	var config Config
-	_, err := toml.DecodeFile(path, &config)
+	err = yaml.Unmarshal(buf, &config)
 	if err != nil {
 		return nil, err
 	}
--- infrastructure/database/sql_handler.go
+++ infrastructure/database/sql_handler.go
@@ -1,1 +1,54 @@
 package database
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+)
+
+type SqlHandler struct {
+	DB *gorm.DB
+}
+
+func NewSqlHandler(driver, url string) (*SqlHandler, error) {
+	db, err := gorm.Open(driver, url)
+	if err != nil {
+		return nil, err
+	}
+	return &SqlHandler{
+		DB: db,
+	}, nil
+}
+
+func (s *SqlHandler) Close() {
+	if err := s.DB.Close(); err != nil {
+		panic(err)
+	}
+}
+
+func (s *SqlHandler) Insert(value interface{}) error {
+	if err := s.DB.Create(value).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SqlHandler) Update(value interface{}) error {
+	if err := s.DB.Save(value).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SqlHandler) SelectById(value interface{}) error {
+	if !s.DB.First(value).Limit(1).RecordNotFound() {
+		return errors.New("record not found.")
+	}
+	return nil
+}
+
+func (s *SqlHandler) Select(value interface{}) error {
+	if err := s.DB.Find(value).Error; err != nil {
+		return err
+	}
+	return nil
+}
